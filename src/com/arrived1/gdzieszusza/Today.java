e9793079e4cdbb24c4d35a43704c2e74228f1f53
Radoslaw Przybyl <radoslaw.przybyl@nsn.com>
2013-02-16 15:42:39 +0100

    added geolocalization

--- src/com/arrived1/gdzieszusza/Today.java
+++ src/com/arrived1/gdzieszusza/Today.java
@@ -1,5 +1,14 @@
 package com.arrived1.gdzieszusza;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.view.LayoutInflater;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,7 +22,24 @@ public class Today extends Tab{
 		
 		TextView cityText = (TextView)view.findViewById(R.id.cityString);
 
-		cityText.setText(" " + city.getName());
+		LocationManager locMgr = (LocationManager)view.getContext().getSystemService(Context.LOCATION_SERVICE);
+		Location loc = locMgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		
+		Geocoder gcd = new Geocoder(view.getContext(), Locale.getDefault());
+		List<Address> addresses = null;
+		
+		try {
+			addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (addresses.size() > 0) 
+		    System.out.println(addresses.get(0).getLocality());
+		
+		cityText.setText(" " + addresses.get(0).getLocality());
+//		cityText.setText(" " + city.getName());
 		
 		TodayListAdapter adapter = new TodayListAdapter(view.getContext(), city.getTodayData());
 		ListView listView = (ListView)view.findViewById(R.id.listView);
