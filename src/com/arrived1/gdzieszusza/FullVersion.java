3e93a7971955ba497211cdece5cf9c779d0c5bea
arrived1 <arrived1@gmail.com>
2013-02-17 14:46:33 +0100

    implementation of FullVersion class

--- src/com/arrived1/gdzieszusza/FullVersion.java
+++ src/com/arrived1/gdzieszusza/FullVersion.java
@@ -7,12 +7,23 @@ public class FullVersion extends Tab {
 
 	public FullVersion(LayoutInflater inflater_, int layout) {
 		super(inflater_, layout);
-	
 		
-		TextView title = (TextView)view.findViewById(R.id.tvTitle);
-		TextView description = (TextView)view.findViewById(R.id.tvdesc);
-	
-		title.setText("Druga strona");
-		description.setText("bdsjflk sfkljsdfl skdfj lsdfjlsdf j sidfhsdhj fzkdhjf'zkl xdfgzkjl'fzfgdfg DUPA");
+		TextView fullVerTitle = (TextView)view.findViewById(R.id.appFullVersionName);
+		fullVerTitle.setText("Gdzie Suszą+");
+		
+		TextView fullVerString = (TextView)view.findViewById(R.id.availableOptions);
+		fullVerString.setText("Pełna wersja zawiera:");
+		
+		String txt = "- ostrzega przed fotoradarami na ulicy gdzie prawdopodobnie jest fotoradar" +
+				     "- ostrzega o przekroczeniu prędkości na ulicy gdzie jest fotoradar,\n" +
+				     "- aplikacja może działać w tle,\n" +
+					 "- szereg opcji konfiguracyjnych aplikację,\n" +
+					 "- więcej dni z planem ustawienia fotoradarów,\n" +
+					 "- możliwość segregowania wyników(po nazwie ulicy lub dacie),\n" + 
+					 "- po wybraniu ulicy, aplikacja otwiera mapę,\n" +
+					 "- aplikacja oszczędza baterię\n";
+		
+		TextView fullVerDescribtion = (TextView)view.findViewById(R.id.availableOptionsList);
+		fullVerDescribtion.setText(txt);
 	}
 }
