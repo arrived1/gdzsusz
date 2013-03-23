aa3b703070adcd27d4d7aeaf0c29fbc55b890030
arrived1 <arrived1@gmail.com>
2013-03-23 16:43:38 +0100

    added forgotten files

---
+++ src/com/arrived1/gdzieszusza/CityManulaPickerActivity.java
@@ -0,0 +1,15 @@
+package com.arrived1.gdzieszusza;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CityManulaPickerActivity extends Activity {
+	
+	  @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.city_manual_picker); 
+	        
+	  }
+
+}
